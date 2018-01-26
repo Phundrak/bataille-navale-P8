f965f18203621c395d4912f2a2ae14474555f1ea
nbouteme <nbouteme@users.noreply.github.com>
2018-01-26 08:46:47 +0100

    Ajoute des détails pour l'implémentation du role d'un joueur

--- include/player.h
+++ include/player.h
@@ -8,6 +8,8 @@ typedef struct game_state_s game_state_t;
 
 typedef struct player_s {
 	point_t (*get_action)(struct player_s *, game_state_t *);
+	void (*setup_boats)(struct player_s *, game_state_t *);
+	point_t owned_rect[2];
 	int is_alive;
 } player_t;
 
--- src/player.c
+++ src/player.c
@@ -5,7 +5,7 @@ typedef struct {
 	player_t base;
 } local_player_t;
 
-point_t playerLocalAction(player_t *self, game_state_t *game) {
+static point_t playerLocalAction(player_t *self, game_state_t *game) {
 	/* 
 	   TODO: 
 	   Afficher l'état du jeu
@@ -14,8 +14,66 @@ point_t playerLocalAction(player_t *self, game_state_t *game) {
 	return (point_t){0, 0};
 }
 
+static void playerLocalSetBoats(player_t *self, game_state_t *game) {
+	local_player_t *p = (void*)self;
+	
+}
+
 player_t *newLocalPlayer() {
 	local_player_t *ret = malloc(sizeof(*ret));
 	ret->base.get_action = playerLocalAction;
+	ret->base.setup_boats = playerLocalSetBoats;
 	return &ret->base;
 }
+
+char pieces[][5][5] = {
+	{
+		{1, 1, 0, 0, 0},
+		{1, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	},
+	{
+		{1, 1, 1, 1, 0},
+		{1, 1, 1, 1, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	},
+	{
+		{1, 1, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	},
+	{
+		{1, 1, 1, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	},
+	{
+		{1, 1, 1, 1, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	},
+	{
+		{1, 1, 1, 1, 1},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	},
+	{
+		{1, 1, 1, 1, 0},
+		{0, 0, 0, 1, 0},
+		{0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+	}
+};
