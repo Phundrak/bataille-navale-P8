7950c7f605dfd0b1ba3e2b18d074dd1355bf7c11
Phuntsok Drak-pa <luccar25@gmail.com>
2018-02-09 13:49:55 +0100

    Added Doxygen file in ./doc and documented darray_t related elements

--- include/darray.h
+++ include/darray.h
@@ -1,24 +1,51 @@
+/**
+ * \file darray.h
+ * \brief Implémentation de \ref darray_t et déclaration des fonctions pour ce type
+ */
+
 #ifndef DARRAY_H
 #define DARRAY_H
 
 #include <stdlib.h>
 
+/**
+ * \struct darray_t
+ * \brief Tableau dynamique
+ *
+ * Les objets `darray_t` offrent la possibilité d’avoir des tableaux à taille variable en C, similairement aux objets
+ * `vector` en C++.
+ */
 typedef struct {
-	void *begin;
-	void *end;
-	size_t element_size;
-	size_t capacity;
+	void *begin; /*!< Pointeur sur le premier élément du tableau */
+	void *end; /*!< Pointeur sur l’élément situé immédiatement après le dernier élément du tableau */
+	size_t element_size; /*!< Taille des éléments stockés dans le tableau */
+	size_t capacity; /*!< Capacité maximale du tableau actuel (non destinée à l’utilisateur) */
 } darray_t;
 
+/// \brief Créé un nouvel objet \ref darray_t vide
 darray_t *darrayNew(size_t element_size);
 
+/// \brief Insère un élément à l’endroit pointé dans un \ref darray_t
 void  darrayInsert(darray_t *self, void *pos, void *elem);
+
+/// \brief Supprime l’élément pointé dans l’objet \ref darray_t
 void  darrayErase(darray_t *self, void *pos);
+
+/// \brief Retourne l’élément du \ref darray_t au idx-ème index
 void *darrayGet(darray_t *self, size_t idx);
-size_t darraySize(darray_t *self);
-size_t darrayElemSize(darray_t *self);
+
+/// \brief Insère un élément à la fin de l’élément \ref darray_t
 void  darrayPushBack(darray_t *self, void *elem);
+
+/// \brief Supprime le dernier élément de l’élément \ref darray_t
 void  darrayPopBack(darray_t *self);
+
+/// \brief Détruit l’élément \ref darray_t
 void  darrayDelete(darray_t *self);
+
+size_t darraySize(darray_t *self);
+
+/// \brief Renvoie la taille de l’élément \ref darray_t
+size_t darrayElemSize(darray_t *self);
 
 #endif /* DARRAY_H */
--- src/darray.c
+++ src/darray.c
@@ -1,10 +1,24 @@
+/**
+ * \file darray.c
+ * \brief Implémentation des fonctions pour le type \ref darray_t
+ */
+
 #include <darray.h>
 #include <malloc.h>
 #include <string.h>
 
 #define _GNU_SOURCE
+
+/// Constante pour les erreurs liées à \ref darray_t
 #define PTR_ERROR 2
 
+/**
+ * `darrayNew` permet de créer un nouvel objet de type \ref darray_t ne contenant aucun élément. Le seul paramètre,
+ * `element_size`, est utilisé afin de connaître l’espace mémoire à allouer à chacun des éléments dans le tableau.
+ * Cela implique qu’un objet \ref darray_t ne peut contenir que des éléments du même type.
+ * @param element_size Taille des objets stockés
+ * @return Pointeur sur le nouvel objet \ref darray_t
+ */
 darray_t *darrayNew(size_t element_size) {
 	darray_t *ret;
 	ret = (darray_t*)malloc(sizeof(darray_t));
@@ -15,6 +29,14 @@ darray_t *darrayNew(size_t element_size) {
 	return ret;
 }
 
+/**
+ * `darrayInsert` insère l’élément `elem` à l’endroit pointé par `pos` dans l’objet \ref darray_t. Cela décalera tous
+ * les éléments stockés dans `self` d’un cran vers la fin du tableau et insérera à l’endroit pointé le nouvel élément.
+ * Cette fonction modifie les membres `begin` et `end` et potentiellement `capacity` de `self`.
+ * @param self Objet \ref darray_t dans lequel on souhaite insérer un nouvel élément
+ * @param pos Position à laquelle on souhaite insérer un nouvel élément
+ * @param elem Élément que l’on souhaite insérer
+ */
 void darrayInsert(darray_t *self, void *pos, void *elem) {
 	void *new_array;
 	char *itr;
@@ -40,31 +62,69 @@ void darrayInsert(darray_t *self, void *pos, void *elem) {
 	self->end = (char*)self->end + self->element_size;
 }
 
+/**
+ * `darrayErase` supprime l’élément de objet \ref darray_t `self` pointé par `pos`. Cela décalera tous les éléments
+ * suivants dans le tableau d’un cran vers le début du tableau de manière à ce qu’il n’y aie pas d’élément vide entre
+ * les membres `begin` et `end` de `self`. Par ailleurs, le membre `end` de `self` se retrouve modifié par la fonction.
+ * @param self Objet \ref `darray_t` dont on souhaite supprimer un élément
+ * @param pos Élément de `self` que l’on souhaite supprimer
+ */
 void darrayErase(darray_t *self, void *pos) {
 	memmove(pos, (char *)pos + self->element_size,
 					((char *)self->end - self->element_size) - (char *)pos);
 	self->end = (char*)self->end - self->element_size;
 }
 
-size_t darraySize(darray_t *self) {
-	return ((char*)self->end - (char*)self->begin) / self->element_size;
-}
-
-void *darrayGet(darray_t *self, size_t idx) {
-	void *itr;
-	itr = (char*)self->begin + idx * self->element_size;
-	return itr;
-}
-
+/**
+ * `darrayPushBack` ajoute un nouvel élément `elem` à l’objet `self` à la fin du tableau de ce dernier. Cette
+ * fonction modifie le membre `end` de `self`.
+ * @param self Objet \ref darray_t à la fin duquel on souhaite ajouter un nouvel élément
+ * @param elem Élément que l’on souhaite ajouter à la fin de `self`
+ */
 void darrayPushBack(darray_t *self, void *elem) {
 	darrayInsert(self, self->end, elem);
 }
 
+/**
+ * `darrayPopBack` permet de supprimer le dernier élément de l’objet \ref darray_t passé en argument. Cette fonction
+ * modifie le membre `end` de ce dernier objet.
+ * @param self Objet dont on souhaite supprimer le dernier élément
+ */
 void darrayPopBack(darray_t *self) {
 	darrayErase(self, (char*)self->end - self->element_size);
 }
 
+/**
+ * `darrayDelete` supprime tous les éléments contenus par l’objet \ref darray_t passé en argument avant de libérer la
+ * mémoire occupée par l’objet lui-même. L’objet passé en argument ne sera plus utilisable après utilisation de cette
+ * fonction.
+ * @param self Objet \ref darray_t à supprimer
+ */
 void darrayDelete(darray_t *self) {
 	free(self->begin);
 	free(self);
+}
+
+/**
+ * `darraySize` renvoie le nombre d’éléments contenu dans le \ref darray_t `self` passé en arguments. Cette fonction
+ * ne modifie pas l’élément passé en argument.
+ * @param self Objet \ref darray_t dont on souhaite connaître le nombre d’éléments
+ * @return Nombre d’éléments contenus dans `self`
+ */
+size_t darraySize(darray_t *self) {
+	return ((char*)self->end - (char*)self->begin) / self->element_size;
+}
+
+/**
+ * `darrayGet` permet de récupérer un élément d’un objet \ref darray_t grâce à son index dans le tableau de l’objet
+ * `self`. Si l’index est trop grand, alors le pointeur `NULL` sera renvoyé, sinon un pointeur de type `void*`
+ * pointant sur l’élément correspondant sera renvoyé. Cette fonction ne modifie pas l’objet `self`.
+ * @param self Objet \ref darray_t duquel on souhaite obtenir un pointeur sur l’élément à l’index `idx`
+ * @param idx Index de l’élément que l’on souhaite récupérer
+ * @return Pointeur de type `void*` pointant sur l’élément si l’index est valide, sur NULL sinon.
+ */
+void *darrayGet(darray_t *self, size_t idx) {
+	void *itr;
+	itr = (char*)self->begin + idx * self->element_size;
+	return itr;
 }
